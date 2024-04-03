b2d38364bba4cfc3fb22ef288df73f4a94736c01
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-04-03 15:37:18 +0530

    Files Addded

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 import express  from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+//configur cors usind app.use
+//we can also do some setting inside cors
+//corsOption{ }    is an object 
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"20kb"}))  //form bhara wala data liya
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+
 
 const app=express()
 
